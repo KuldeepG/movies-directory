bc000cbdbb177de0e3d4f652bb611fe961833b35
Kuldeep Ghogre <Kuldeep@INkuldeepg.local>
2014-04-09 00:57:53 +0530

    Basic routing and templating

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 var moviesApp = angular.module('moviesApp',['ngRoute', 'moviesControllers']);
+
+moviesApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/list', {
+		templateUrl: './templates/movieList.html',
+		controller: 'MovieListController'
+	}).otherwise({
+		redirectTo: '/list'
+	});
+}]);
